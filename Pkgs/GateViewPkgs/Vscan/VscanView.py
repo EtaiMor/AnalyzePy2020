eba8c8d52675f714c998e2c0a7dc0fa5b5b68bc7
Itay <itaymo@soreq.gov.il>
2021-01-28 13:02:50 +0200

    add time.sleep between visualization of mayavi to setting its parent to VscanView

--- Pkgs/GateViewPkgs/Vscan/VscanView.py
+++ Pkgs/GateViewPkgs/Vscan/VscanView.py
@@ -15,20 +15,28 @@ from mayavi.tools.mlab_scene_model import MlabSceneModel
 from mayavi.core.ui.mayavi_scene import MayaviScene
 from traitsui.api import View, Item
 from tvtk.pyface.scene_editor import SceneEditor
-
+from mayavi.modules.image_plane_widget import ImagePlaneWidget
 
 class Visualization(HasTraits):
     scene = Instance(MlabSceneModel, ())
     data = Instance(np.ndarray, ())
-
-    @on_trait_change('scene.activated')
-    def update_plot(self):
-         self.scene.mlab.volume_slice(self.data)
-
-
+    x_plane = Instance(ImagePlaneWidget, ())
+    y_plane = Instance(ImagePlaneWidget, ())
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
                      height=25, width=300, show_label=False),
                 resizable=True)  # We need this to resize with the parent widget
+
+    @on_trait_change('scene.activated')
+    def update_plot(self):
+        self.x_plane = self.scene.mlab.volume_slice(self.data, plane_orientation='x_axes')
+        self.y_plane = self.scene.mlab.volume_slice(self.data, plane_orientation='z_axes')
+        print('im here')
+
+    def update(self, new_data):
+        self.data = new_data
+        self.x_plane.mlab_source.scalars = self.data
+        self.y_plane.mlab_source.scalars = self.data
+        print(self.data.shape)
 
 
 class MayaviQWidget(QtGui.QWidget):
@@ -39,7 +47,6 @@ class MayaviQWidget(QtGui.QWidget):
         layout.setSpacing(0)
         self.visualization = Visualization()
         self.visualization.data = data
-
         self.ui = self.visualization.edit_traits(parent=self, kind='subpanel').control
         time.sleep(1)
         self.ui.setParent(self)
@@ -63,38 +70,13 @@ class VscanView(Dock):
         self.gate_docview: GateDocView = vscan_docview.gate_docview
         container = QtGui.QWidget()
         self.maya_widget = MayaviQWidget(self.vscan_docview.v_scan, container)
-        # self.layout.addWidget(self.maya_widget, 1, 1)
         self.addWidget(self.maya_widget)
         container.show()
         vscan_docview.vscan_changed_event += self.on_vscan_changed
 
 
     def on_vscan_changed(self, v_scan: np.ndarray):
-        (num_row, num_col, wave_len) = v_scan.shape
-        self.src.scalar_data = v_scan
-        self.src.update()
-        # if (mlab.get_engine() is None):
-        #     mlab.figure('Volume Scan')
-        #     mlab.show()
-        #
-        # mlab.clf(self.scene)
-        # # mlab.contour3d(v_scan)
-        # mlab.volume_slice(v_scan)
-
-
-    # def set_image_item(self, cscan):
-    #     self.image_view.imageItem.setImage(cscan)
-
-    # def mouseClickEvent(self, event):
-    #     i_indx = int(event.pos().y())
-    #     j_indx = int(event.pos().x())
-    #     gate_docview: GateDocView = self.cscan_docview.gate_docview
-    #     gate_docview.update_ij_pos(i_indx, j_indx)
-    #
-    # def mouseMoved(self, viewPos):
-    #     scenePos = self.image_view.getImageItem().mapFromScene(viewPos)
-    #     row, col = int(scenePos.y()), int(scenePos.x())
-    #     self.cscan_docview.set_mouse_ij_pos(row, col)
+        self.maya_widget.visualization.update(v_scan)
 
 
 
